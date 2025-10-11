a3bbb88beca85b67303fc7767065cac0fe08fe0a
Himani Bisht <himanibisht9193@gmail.com>
2025-10-11 19:37:51 +0530

    try to fix bug

--- src/components/AdminDashboard.jsx
+++ src/components/AdminDashboard.jsx
@@ -1,6 +1,6 @@
 import React, { useState, useEffect } from 'react';
 import axios from 'axios';
-import './Dashboard.css';
+import './dashboard.css';
 import { getApiUrl, API_ENDPOINTS } from '../config';
 
 const AdminDashboard = ({ user, onLogout }) => {
